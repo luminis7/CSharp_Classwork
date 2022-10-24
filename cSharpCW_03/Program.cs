efb91df9f2e2c76b21d2cffc18239a1f662effd2
luminis7 <luminis@inbox.ru>
2022-10-24 22:22:12 +0300

    add thirdth classwork

---
+++ cSharpCW_03/Program.cs
@@ -0,0 +1,26 @@
+﻿// Напишите программу, которая по заданному номеру четверти, показывает диапазон возможных координат точек в этой четверти (x и y).
+/*
+void ShowQuart(int quartNum)
+{
+    if(quartNum >= 1 && quartNum <= 4)
+    {
+        if(quartNum ==1) Console.WriteLine("x > 0 and y > 0");
+        else if(quartNum == 2) Console.WriteLine("x < 0 and y > 0");
+        else if(quartNum == 3) Console.WriteLine("x < 0 and y < 0");
+        else Console.WriteLine("x > 0 and y < 0");
+    }
+    else
+    Console.WriteLine("Wrong");
+   
+}
+
+Console.Write("Input a nimber of quart: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+ShowQuart(num);
+*/
+// Напишите программу, которая принимает на вход координаты точки (X и Y), причём X ≠ 0 и Y ≠ 0 и выдаёт номер четверти плоскости, в которой находится эта точка.
+
+// Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 2D пространстве.
+
+// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу квадратов чисел от 1 до N.
